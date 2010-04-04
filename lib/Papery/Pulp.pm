9f46d6948dd575f811a5b0869557b7e0e96c4148
Philippe Bruhat (BooK) <book@cpan.org>
2010-04-04 15:36:51 +0200

    Papery::Pulp: add the steps-handling code
    For each step, the Papery::Pulp object picks the corresponding handler
    class (subclass of Papery::Analyzer, Papery::Processor and Papery::Renderer),
    creates a new instance with the given options, and then call the handler
    passing itself as a parameter.

--- lib/Papery/Pulp.pm
+++ lib/Papery/Pulp.pm
@@ -13,5 +13,46 @@ sub new {
 
 sub merge_meta { Papery::Util::merge_meta( $_[0]->{meta}, $_[1] ); }
 
+#
+# Steps handlers
+#
+
+# utility method
+sub _class_args {
+    my ( $self, $which ) = @_;
+
+    # compute the base class name
+    my $base = $which;
+    $base =~ s/^_//;
+    $base = 'Papery::' . ucfirst $base;
+
+    # get the values from the meta
+    $which = $self->{meta}{$which};
+    my ( $class, @args ) = ref $which eq 'ARRAY' ? @{$which} : $which;
+
+    return "$base\::$class", @args;
+}
+
+sub analyze_file {
+    my ( $self,  $file )    = @_;
+    my ( $class, @options ) = $self->_class_args('_analyzer');
+    eval "require $class";
+    return $class->new(@options)->analyze_file( $self, $file );
+}
+
+sub process {
+    my ($self) = @_;
+    my ( $class, @options ) = @{ $self->{meta}{_processor} };
+    eval "require $class";
+    return $class->new(@options)->process($self);
+}
+
+sub render {
+    my ($self) = @_;
+    my ( $class, @options ) = @{ $self->{meta}{_renderer} };
+    eval "require $class";
+    return $class->new(@options)->render($self);
+}
+
 1;
 
