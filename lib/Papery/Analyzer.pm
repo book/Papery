9531a1f360a87daeb10f12e202dc84614d8adc5a
Philippe Bruhat (BooK) <book@cpan.org>
2010-04-07 20:43:36 +0200

    Papery::Analyzer: analyzer() is a class method, and takes options
    No more constructor needed.

--- lib/Papery/Analyzer.pm
+++ lib/Papery/Analyzer.pm
@@ -3,17 +3,14 @@ package Papery::Analyzer;
 use strict;
 use warnings;
 
-sub new {
-    my ( $class, @args ) = @_;
-    return bless {@args}, $class;
-}
-
 sub analyze_file {
-    my ( $self, $pulp, $file ) = @_;
+    my ( $class, $pulp, $file, @options ) = @_;
+
     open my $fh, $file or die "Can't open $file: $!";
     local $/;
     my $text = <$fh>;
     close $fh;
+
     $pulp->{meta}{__file} = $file;
     return $self->analyze( $pulp, $text );
 }
