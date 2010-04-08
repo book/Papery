71cee2e4b98c36708e3d5be4e12967fcaea80bbd
Philippe Bruhat (BooK) <book@cpan.org>
2010-04-08 20:57:44 +0200

    Papery::Render::Template: a renderer for Template Toolkit
    $Template::Stash::PRIVATE must be set to undef so that the templates
    can see "private" variables like _content.
    
    Since Template::Stash is required at run time, we have to wait until
    it's loaded (i.e. after the call to Template->new()) before localizing
    $Template::Stash::PRIVATE.

---
+++ lib/Papery/Renderer/Template.pm
@@ -0,0 +1,29 @@
+package Papery::Renderer::Template;
+
+use strict;
+use warnings;
+
+use Papery::Renderer;
+our @ISA = qw( Papery::Renderer );
+
+use Template;
+
+sub render {
+    my ( $class, $pulp, @args ) = @_;
+    my $template
+        = Template->new( @args, INCLUDE_PATH => $pulp->{meta}{_templates} )
+        or die Template->error();
+
+    die "No _layout for $pulp->{meta}{__source_file}"
+        if !$pulp->{meta}{_layout};
+
+    local $Template::Stash::PRIVATE;    # we want to see "private" vars
+    $template->process( $pulp->{meta}{_layout},
+        $pulp->{meta}, \( $pulp->{meta}{__output} = '' ) )
+        or die $template->error;
+
+    return $pulp;
+}
+
+1;
+
