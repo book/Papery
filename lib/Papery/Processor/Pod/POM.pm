0f0690b5d220bd1c07eda7528773a95170e38598
Philippe Bruhat (BooK) <book@cpan.org>
2010-04-11 16:27:22 +0200

    Papery::Processor::Pod::POM: documentation

--- lib/Papery/Processor/Pod/POM.pm
+++ lib/Papery/Processor/Pod/POM.pm
@@ -37,3 +37,60 @@ sub process {
 
 1;
 
+__END__
+
+=head1 NAME
+
+Papery::Processor::Pod::POM - Papery processor based on Pod::POM
+
+=head1 SYNOPSIS
+
+    # _config.yml
+    _processors:
+      pod: Pod::POM
+
+    # metadata
+    _processor: Pod::POM
+
+=head1 DESCRIPTION
+
+C<Papery::Processor::Pod::POM> will process the C<_text> of a C<Papery::Pulp>
+object as POD, and put HTML in the C<_content>, using C<Pod::POM> for
+parsing the POD, and by default C<Pod::POM::View::HTML> to turn it into
+HTML.
+
+=head1 METHODS
+
+This class provides a single method:
+
+=over 4
+
+=item process( $pulp )
+
+Analyze the C<_text> metadata, and update the C<$pulp> metadata and
+C<_content>.
+
+If the C<pod_pom_view> metadata key exists, it is assumed to contain
+the name of a C<Pod::POM::View> class, that will be used instead to
+produce the C<_content> metadata.
+
+The intermediate C<Pod::POM> object returned by the C<Pod::POM> parser
+is stored in the C<_pod_pom> metadata key.
+
+=back
+
+=head1 AUTHOR
+
+Philippe Bruhat (BooK), C<< <book at cpan.org> >>
+
+=head1 COPYRIGHT
+
+Copyright 2010 Philippe Bruhat (BooK), all rights reserved.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
