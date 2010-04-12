579fa77d61b7693716cbca9f93966d62e0e7a7a0
Philippe Bruhat (BooK) <book@cpan.org>
2010-04-12 09:54:51 +0200

    Papery::Renderer::Template: documentation

--- lib/Papery/Renderer/Template.pm
+++ lib/Papery/Renderer/Template.pm
@@ -27,3 +27,49 @@ sub render {
 
 1;
 
+__END__
+
+=head1 NAME
+
+Papery::Renderer::Template - Papery renderer based on Template
+
+=head1 SYNOPSIS
+
+    # _config.yml
+    _renderer: Template
+
+    # metadata
+    _renderer: Template
+
+=head1 DESCRIPTION
+
+C<Papery::Renderer::Temlate> will render the C<_content> of a C<Papery::Pulp>
+object with C<Template>, and put the result  in the C<_output>.
+
+=head1 METHODS
+
+This class provides a single method:
+
+=over 4
+
+=item render( $pulp )
+
+Simply proces the template defined in C<_layout> to produce the C<_output>.
+
+=back
+
+=head1 AUTHOR
+
+Philippe Bruhat (BooK), C<< <book at cpan.org> >>
+
+=head1 COPYRIGHT
+
+Copyright 2010 Philippe Bruhat (BooK), all rights reserved.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
